c7b2783e8f0aa8d3139d2af6fc36b27eff9a29a7
kadir-zeren <k4dzeren@gmail.com>
2024-02-18 16:50:51 +0300

    list

--- index.py
+++ index.py
@@ -1,1 +1,36 @@
-print('Hello')
+listem = ['a','b','c','d','e']
+print(type(listem))
+
+word = 'happy'
+print(list(word))
+print(list('a b c'))
+
+country = ['USA','BRAZIL','UK']
+print(country)
+
+text = 'Clarusway'
+print(list(text))
+print([text])
+
+listem = [1,2,3]
+print(len(listem))
+print(list(listem))
+
+a= [[1,2,3],['ali','ayse'],[True,False]]
+print(len(a))
+
+text="2024's perfect"
+print(list(text))
+print([text])
+#list icine iterebil alir
+bos1=[]
+print(len(bos1))
+
+numbers= [1,4,7]
+numbers.append(9)
+print(numbers)
+numbers.append('9')
+print(numbers)
+numbers.append(['a','b','c'])
+print(numbers)
+
